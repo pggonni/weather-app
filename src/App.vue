4f09e289863655ac876dbb4bdf43dac4fa8af9f1
Pg Gonni <59372630+pggonni@users.noreply.github.com>
2021-03-17 18:00:26 -0400

    Adding code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form>
+    <div>
+      <label>City</label>
+      <input v-model.trim="City">
+    </div>
+    <button type="submit">Go</button>
+  </form>
+    <div>
+      <div>
+        <p>Feels like</p>
+        <p>{{ .feels_like }}</p>
+      </div>
+      <div>
+        <p>Humidity</p>
+        <p>{{ result.humidity }}</p>
+      </div>
+      <div>
+        <p>Pressure</p>
+        <p>{{ results.pressure }}</p>
+      </div>
+      <div>
+        <p>Temperature</p>
+        <p>{{ results.temp }}</p>
+      </div>
+      <div>
+        <p>High</p>
+        <p>{{ results.temp_max }}</p>
+      </div>
+      <div>
+        <p>Low</p>
+        <p>{{ results.temp_min }}</p>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const APIKEY = "key";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      city: "",
+      results: {}
+    };
+  },
+  methods: {
+    async getWeather() {
+      if (!this.city) {
+        return
+      } 
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${APIKEY}`
+      );
+      const { main } = await res.json();
+      this.results = main;
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 </style>
